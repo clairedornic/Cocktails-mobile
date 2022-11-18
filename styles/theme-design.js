3c24a5a48b590a9e97234ffd5eabd0ef9fb61711
clairedornic <50169132+clairedornic@users.noreply.github.com>
2022-11-18 12:04:08 +0100

    STYLE: add a design theme

---
+++ styles/theme-design.js
@@ -0,0 +1,30 @@
+const theme = {
+    'colors' : {
+        'white' : '#fff',
+        'black' : '#000',
+        'lightBlue' : '#D6ECEC',
+    },
+    'spacing' : {
+        'extraLarge' : 35,
+        'large' : 25,
+        'medium' : 20,
+        'small' : 15,
+        'extraSmall' : 10,
+        'thin' : 8,
+        'none' : 0,
+    },
+    'fontSize' : {
+        'large' : 24,
+        'medium' : 20,
+        'small' : 16,
+    },
+    'size' : {
+        'full' : '100%',
+    },
+    'radius' : {
+        'medium' : 20, 
+        'small' : 15,
+    }
+}
+
+export default theme;
